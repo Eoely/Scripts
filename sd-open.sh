088f6475fbfdcce89658176164cd7c3eef1be04c
eoely <eos033@uit.no>
2025-04-13 13:35:10 +0200

    Working first version, seemingly
    Compared with URL generated by portal, and via script and they are identical.

--- sd-open.sh
+++ sd-open.sh
@@ -13,45 +13,33 @@
 # Not sure what is the best approach then,probably just reading its content to a string.
 CONFIG_FILE="$HOME/ngrok.yml"
 
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    echo "Error: Required configuration file not found at '$CONFIG_FILE'" >&2
+    echo "Please create it or check the path." >&2
+    exit 1
+fi
+
+# Copied from the portal with no overrides and master branch selected
+baseUrl="https://testing.smartdok.dev/Loader?next=%2FStart.aspx&web-url=https%3A%2F%2Fsd-master-web.azurewebsites.net&frontend-url=https%3A%2F%2Fsmartdokui.z16.web.core.windows.net%2Fmaster%2F&smartapi-url=https%3A%2F%2Fsd-master-smartapi.azurewebsites.net&mobileapi-url=https%3A%2F%2Fsd-master-mobileapi.azurewebsites.net&smartdokapi-url=https%3A%2F%2Fsd-master-api.azurewebsites.net&luna-url=https%3A%2F%2Fsd-master-lunaapi.azurewebsites.net&bff-url=https%3A%2F%2Fsd-eivind-bff.eu.ngrok.io&overlay-url=https%3A%2F%2Fportal.smartdok.dev%2Foverlay.js&export-url=https%3A%2F%2Fsmartdok-export-master.azurewebsites.net&document-service-url=https%3A%2F%2Fsmartdok-documents-master.azurewebsites.net&document-center-url=https%3A%2F%2Fsd-document-center-master.azurewebsites.net&htmlpdf-url=https%3A%2F%2Fsmartdok-htmlpdf-master.azurewebsites.net&officepdf-url=https%3A%2F%2Fsmartdok-office-pdf-master.azurewebsites.net"
+
 for source in "$@"
 do
-    echo "$source"
     subdomain="$(yq ".tunnels.${source}.subdomain" $CONFIG_FILE)"
     if [[ "$subdomain" = null ]]; then
         echo "Error: Value ${source} not found in config" >&2
         exit 1
     fi
     
-    echo "$subdomain"
-    
     # URL encoded "https://"
     complete="https%3A%2F%2F${subdomain}.eu.ngrok.io"
-    echo "$complete"
+
+    #url paramter representing the override 
+    paramKey="&${source}-url="
+    # Replace the parameter value with the ngrok subdomain
+    # Probably a better way to replace itself than echoing it etcs
+    baseUrl="$(echo $baseUrl | sed "s#\($paramKey\)[^&]*#\1$complete#")"
 done
 
-exit 1
-
-
+echo "$baseUrl"
 
 exit 1
-
-# if [[ ! -f "$CONFIG_FILE" ]]; then
-#     echo "Error: Required configuration file not found at '$CONFIG_FILE'" >&2
-#     echo "Please create it or check the path." >&2
-#     exit 1
-# fi
-
-
-
-# This works seemingly
-baseUrl="https://testing.smartdok.dev/Loader?next=%2FStart.aspx&web-url=https%3A%2F%2Fsd-master-web.azurewebsites.net&frontend-url=https%3A%2F%2Fsmartdokui.z16.web.core.windows.net%2Fmaster%2F&smartapi-url=https%3A%2F%2Fsd-master-smartapi.azurewebsites.net&mobileapi-url=https%3A%2F%2Fsd-master-mobileapi.azurewebsites.net&smartdokapi-url=https%3A%2F%2Fsd-master-api.azurewebsites.net&luna-url=https%3A%2F%2Fsd-master-lunaapi.azurewebsites.net&bff-url=https%3A%2F%2Fsd-eivind-bff.eu.ngrok.io&overlay-url=https%3A%2F%2Fportal.smartdok.dev%2Foverlay.js&export-url=https%3A%2F%2Fsmartdok-export-master.azurewebsites.net&document-service-url=https%3A%2F%2Fsmartdok-documents-master.azurewebsites.net&document-center-url=https%3A%2F%2Fsd-document-center-master.azurewebsites.net&htmlpdf-url=https%3A%2F%2Fsmartdok-htmlpdf-master.azurewebsites.net&officepdf-url=https%3A%2F%2Fsmartdok-office-pdf-master.azurewebsites.net"
-# test="$(echo $baseUrl | sed 's/master/BASTING/g')"
-# echo "$test"
-
-source="bff"
-
-paramKey="&${source}-url="
-echo "$paramKey"
-
-test="$(echo $baseUrl | sed "s#\($paramKey\)[^&]*#\1REPLACED#")"
-echo "$test"
