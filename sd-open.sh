28086bf9d5e11814474a1bb1369b8458efd16e61
Eivind Østlyngen <eivind.ostlyngen@visma.com>
2025-04-13 19:39:51 +0200

    Allow branch selection, -s flag

--- sd-open.sh
+++ sd-open.sh
@@ -2,7 +2,7 @@
 # Requirements: ngrok + config, yq, jq,  sed
 
 #TODO:
-#General code cleanup, especially encode:
+#General code cleanup, especially encoding logic
 #Document center urls are not set?
 
 CONFIG_FILE="$HOME/ngrok.yml"
@@ -15,24 +15,28 @@ fi
 
 baseUrl="https://testing.smartdok.dev/Loader?next=%2FStart.aspx&overlay-url=https%3A%2F%2Fportal.smartdok.dev%2Foverlay.js"
 frontEndUrl="https%3A%2F%2Fsmartdokui.z16.web.core.windows.net%2Fmaster%2F"
-branch="master"
+branch="master" # Default, changed programatically
 
 data="$(curl -sS https://portal.smartdok.dev/environments.json)"
-branchData="$(echo $data | jq --arg branch $branch '.[] | select(.name == $branch)')"
+portalBranches="$(echo $data | jq -r '.[] | .name' | tr -d '\r')"
 
-#Resolve optional branch flag (-b)
-while getopts ":b:" opt; do
+#Resolve flags:
+# -b (branch): Pass in named branch which you will open
+# -s (search): Select among branches available
+while getopts ":b:s" opt; do
     case $opt in
     b)
         # Return error message if branch does not exist
         # This could be iterated to a branch selection in fzf if wanted.
-        portalBranches="$(echo $data | jq -r '.[] | .name')"
         if [[ ! $portalBranches =~ $OPTARG ]]; then
             echo "Error: Branch $OPTARG does not exist" >&2
             echo "Valid branches: $portalBranches" >&2
             exit 1
         fi
         branch=$OPTARG
+        ;;
+    s)
+        branch="$(echo -e $portalBranches | sed 's# #\n#g' | fzf)"
         ;;
     \?)
         echo "Invalid option: -$OPTARG" >&2
@@ -45,6 +49,8 @@ while getopts ":b:" opt; do
     esac
 done
 
+branchData="$(echo $data | jq --arg branch $branch '.[] | select(.name == $branch)')"
+
 # Add UI override if passed, always use localhost.
 # If needed could add support for ngrok url, via different key (smartdokui).
 if [[ $@ =~ "ui" ]]; then
@@ -54,7 +60,7 @@ elif [[ "$branch" != "master" ]]; then
     # Check if the branch has UI created, as it is not listed in "urls".
     # if it does, update frontend Url with branch one
     hasFrontEnd="$(echo $branchData | jq '.branches["smartdok-ui"]')"
-    if [[ ! "$hasFrontEnd" == null ]]; then
+    if [[ "$hasFrontEnd" != null ]]; then
         frontEndUrl="$(echo $frontEndUrl | sed "s#master#$branch#")"
     fi
 fi
@@ -89,9 +95,6 @@ for key in $(echo "$urls" | jq -r 'keys[]' | tr -d '\r'); do
     # "&smartapi-url=https......"
     baseUrl="${baseUrl}&${key,,}-url=${url}"
 done
-
-#Print the baseUrl. Useful for debug.
-# echo $baseUrl
 
 # Open the url in the browser, potentially only windows compatible
 start $baseUrl
